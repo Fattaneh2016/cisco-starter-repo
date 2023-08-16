703fbf2faae3dab13bd2e3ed79f17e6529ef3808
fattaneh2016 <fatameri@hotmail.com>
2023-08-16 18:14:48 -0400

    update banner

--- sextant/src/banner.js
+++ sextant/src/banner.js
@@ -5,7 +5,7 @@ class Banner extends Component {
     render() {
         return (
             <div className="Banner">
-                <img src="https://images.credly.com/images/7bc2a3b9-289e-495b-959c-2bf431b5431d/Cisco_DevNetSpecialist.png" className="logo" alt="Exhibit" style={{ width: '100px', height: '100px' }} />
+                {/* <img src="https://images.credly.com/images/7bc2a3b9-289e-495b-959c-2bf431b5431d/Cisco_DevNetSpecialist.png" className="logo" alt="Exhibit" style={{ width: '100px', height: '100px' }} /> */}
                 <h1>{this.props.bannerText}</h1>
             </div>
         );
